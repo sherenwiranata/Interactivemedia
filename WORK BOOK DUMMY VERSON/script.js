ca8ba45fd1cc69214f89e99d9e00a47e4366aa26
sherenwiranata <swiranata@student.unimelb.edu.au>
2025-03-26 13:21:26 +1100

    deleting the workbook dummy file

--- WORK BOOK DUMMY VERSON/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".gameName").addEventListener("click", function() {
-    this.classList.add("wobble");
-
-    // Remove the class after animation ends so it can be triggered again
-    setTimeout(() => {
-        this.classList.remove("wobble");
-    }, 300); // Match the animation duration
-});
