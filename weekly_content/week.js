c965b1e91dda53c3e26910ce8c435d64485f9176
sherenwiranata <swiranata@student.unimelb.edu.au>
2025-03-26 20:23:34 +1100

    update

--- weekly_content/week.js
+++ weekly_content/week.js
@@ -20,42 +20,63 @@ function placeCoins() {
 window.onload = placeCoins;
 
 //from matilda's workbook
-// Get all the elements with class name "draggable":
-var draggableElements = document.getElementsByClassName("draggable");
-
-// Apply dragElement function to each draggable element:
-for (var i = 0; i < draggableElements.length; i++) {
-  dragElement(draggableElements[i]);
-}
-
+// This function enables dragging functionality
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-  // Attach the drag to the element itself
-  elmnt.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (elmnt.getElementsByClassName(elmnt.className)[0]) {
+      elmnt.getElementsByClassName(elmnt.className)[0].onmousedown = dragMouseDown;
+    } else {
+      elmnt.onmousedown = dragMouseDown;
+    }
+  
+    // Mouse down function to start dragging
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    // Function to move the element as the mouse moves
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+  
+    // Function to stop dragging
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
   }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  
+  // Function to randomly position elements
+  function randomPosition(elmnt) {
+    var randomX = Math.floor(Math.random() * (window.innerWidth - elmnt.offsetWidth)); // Random X position, adjusted by the element's width
+    var randomY = Math.floor(Math.random() * (window.innerHeight - elmnt.offsetHeight)); // Random Y position, adjusted by the element's height
+    elmnt.style.position = 'absolute';
+    elmnt.style.left = randomX + 'px';
+    elmnt.style.top = randomY + 'px';
   }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
+  
+  // Get all elements with the class "draggable" and apply random position and drag functionality
+  var draggableElements = document.getElementsByClassName("draggable");
+  
+  for (var i = 0; i < draggableElements.length; i++) {
+    var element = draggableElements[i];
+    
+    // Apply random position to each element on page load
+    randomPosition(element);
+    
+    // Enable dragging on each element
+    dragElement(element);
   }
-}
+  
