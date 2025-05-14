663a7258fea881bd98abbbf576cc4b18a1166ac6
sherenwiranata <swiranata@student.unimelb.edu.au>
2025-05-15 02:35:59 +1000

    update

--- weekly_content_3/landing/landing.js
+++
@@ -1,8 +0,0 @@
-const cursor = document.createElement('div');
-cursor.className = 'glitch-cursor';
-document.body.appendChild(cursor);
-
-document.addEventListener('mousemove', e => {
-  cursor.style.left = `${e.pageX}px`;
-  cursor.style.top = `${e.pageY}px`;
-});
